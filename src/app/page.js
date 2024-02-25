9f6d343df37b80b2317dea0af584aec06e899b6b
joshypaily <joshypaily@gmail.com>
2024-02-25 21:08:08 +0530

    landing page -inprogress icon added

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,29 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import CircularProgress from '@mui/material/CircularProgress';
+import Grid from '@mui/material/Grid';
+
 
 export default function Home() {
   return (
-    <h1>Comming soon..... Stay tuned....</h1>
+    
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      flexDirection = "row"
+      sx={{ minHeight: '100vh' }}
+    >
+      <Grid item xs={1} >
+        <h1>Inprogress</h1>
+      </Grid>
+      <Grid item xs={1}>
+        <CircularProgress />
+      </Grid>
+    </Grid>
   );
 }
+
+
