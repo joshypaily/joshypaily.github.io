8514bf784386ab641167aff7657ef3e7af3dbcae
joshypaily <joshypaily@gmail.com>
2024-02-25 19:44:58 +0530

    github action trigger

--- src/app/achivements/page.js
+++ src/app/achivements/page.js
@@ -3,7 +3,7 @@ import { Container } from "@mui/material";
 export default function Home() {
   return (
     <Container>
-      My achivements are as follows
+      My achivements are as follows....
     </Container>
   );
 }
