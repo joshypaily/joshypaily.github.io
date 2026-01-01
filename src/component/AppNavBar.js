f210635d2398a1ccda6dcb656d1bcceb675acf85
Joshy Paily <48067999+joshypaily@users.noreply.github.com>
2026-01-02 00:34:29 +0530

    Development (#109)
    * migrate to bootstrap
    
    * completed bootstarap migration
    
    * formatting fix
    
    * achievement page updated
    
    * fix formatting
    
    * content minor changes
    
    * recommendation section design changes
    
    * recommendation section design changes
    
    * completed bootstarap migration
    
    * formatting fix
    
    * achievement page updated
    
    * content minor changes
    
    * recommendation section design changes
    
    * addedm few achievement pics
    
    * merge code fix

--- src/component/AppNavBar.js
+++ src/component/AppNavBar.js
@@ -49,9 +49,6 @@ export default function AppAppBar() {
   // Collapse navbar after clicking a link
   const handleNavClick = () => setExpanded(false);
 
-  // Collapse navbar after clicking a link
-  const handleNavClick = () => setExpanded(false);
-
   return (
     <Navbar
       expand="lg"
