3faf7834ea13e2db0dd978c51a6acfa4432df093
joshypaily <joshypaily@gmail.com>
2026-01-02 00:24:21 +0530

    merge code fix

--- src/component/AppNavBar.js
+++ src/component/AppNavBar.js
@@ -45,9 +45,7 @@ export default function AppAppBar() {
     localStorage.setItem("theme", newTheme);
     document.body.className = newTheme === "dark" ? "bg-dark text-light" : "bg-light text-dark";
   };
-  // Collapse navbar after clicking a link
-  const handleNavClick = () => setExpanded(false);
-
+  
   // Collapse navbar after clicking a link
   const handleNavClick = () => setExpanded(false);
 
